9fc031cf993ae5526c763482da3eb873c4081a4c
Théo <64200915+Magic-80@users.noreply.github.com>
2024-01-24 11:55:02 +0100

    Update projet to react

---
+++ Cv/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import Header from './assets/layout/Header/Header'
+function App() {
+ 
+  return (
+    <>
+      <Header/> 
+    </>
+  )
+}
+
+export default App
---
+++ Cv/src/assets/layout/Header/Header.jsx
@@ -0,0 +1,22 @@
+function Header()
+{
+    return(
+        <>
+            <header>
+                <nav>
+                    <ul>
+                        <li> Logo </li>
+                        <li> <span> Deruelle </span> Théo </li>
+                        <li> Acceuil </li>
+                        <li> Présentation  </li>
+                        <li> Compétence </li>
+                        <li> Projet </li>
+                        <li> Contact </li>
+                    </ul>
+                </nav>
+            </header>
+        </>
+    )
+}
+
+export default Header
