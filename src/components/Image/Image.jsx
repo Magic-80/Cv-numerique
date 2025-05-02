e4eb09c1ac4f179301459d4b81d1a98804dffc0e
Magic-80 <developper.magic@gmail.com>
2025-05-02 21:20:03 +0200

    Feat: - Remise à niveaux

--- src/components/Image/Image.jsx
+++
@@ -1,9 +0,0 @@
-// eslint-disable-next-line react/prop-types
-function Image({className , src , alt})
-{
-    return (
-        <img className={className} src={src} alt={alt} />
-    )
-}
-
-export default Image
